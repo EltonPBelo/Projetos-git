ee44cac5ce0f04b0e038e37d35fbbf1b55f910ee
Elton <eltonpbelo01@gmail.com>
2023-08-28 09:52:34 -0300

    mudança no código

--- Curso de Java Dio/src/CondicoesTernarias.java
+++ Curso de Java Dio/src/CondicoesTernarias.java
@@ -1,9 +1,12 @@
 public class CondicoesTernarias {
     public static void main(String[] args) {
-        int nota = 5;
+        int nota = 4;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        //Vai analisar a nota e vai dizer se está aprovado ou não, nesse caso está reprovado
+        String resultado = nota >= 7 ? "Aprovado" : (nota >= 5 && nota < 7 ? "Recuperação" :  "Reprovado");
+
         System.out.println(resultado);
+
     }
     
 }
