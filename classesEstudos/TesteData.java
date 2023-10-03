9308a866d125e8d42a9ee86746d59047e0a38675
Elton <eltonpbelo01@gmail.com>
2023-10-03 09:31:35 -0300

    aaaaa

--- classesEstudos/TesteData.java
+++ classesEstudos/TesteData.java
@@ -1,5 +1,19 @@
 package classesEstudos;
 
+import java.util.Scanner;
+
 public class TesteData {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Data Calendario = new Data();
+        Calendario.dia = input.nextInt();
+        Calendario.mes = input.nextLine();
+        Calendario.ano = input.nextInt();
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+       
+    }
 }
