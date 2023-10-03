2c7507f1c2b96963b0d0c3c4388755896a4fd991
Elton <eltonpbelo01@gmail.com>
2023-10-03 09:39:24 -0300

    mudanca no codigo

--- classesEstudos/TesteData.java
+++ classesEstudos/TesteData.java
@@ -8,12 +8,16 @@ public class TesteData {
 
         Data Calendario = new Data();
         Calendario.dia = input.nextInt();
+        input.nextLine();//input vazio pra nao dar erro de new line consuming(que vai ignorar o meu input String mes e vai direto pro ano)
+        System.out.println("--------------------------------------");
         Calendario.mes = input.nextLine();
         Calendario.ano = input.nextInt();
         
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("Dia: "+ Calendario.dia);
+        System.out.println("Do mês de: "+Calendario.mes);
+        System.out.println("Do ano de : "+Calendario.ano);
+
+        input.close();
        
     }
 }
