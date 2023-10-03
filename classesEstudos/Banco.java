ae7a185fa74254c0a1f6007a4592660c743fa395
Elton <eltonpbelo01@gmail.com>
2023-10-03 10:19:46 -0300

    banco

---
+++ classesEstudos/Banco.java
@@ -0,0 +1,11 @@
+package classesEstudos;
+
+public class Banco {
+
+    String nome;
+    String agencia;
+    Double saldo = 3200.00;
+    int conta;
+
+    
+}
---
+++ classesEstudos/BancoTeste.java
@@ -0,0 +1,30 @@
+package classesEstudos;
+
+import java.util.Scanner;
+
+public class BancoTeste {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    Banco Bradesco = new Banco();
+
+    System.out.print("Olá bem vindo ao Bradesco, diga o seu nome: ");
+    Bradesco.nome = input.nextLine();
+    System.out.println("Bem vindo a agência : "+Bradesco.nome);
+
+    System.out.println("----------------------------------------------");
+
+    //Sacar dinheiro
+    System.out.printf("Qual o valor voçê deseja  sacar, lembrando que voçê possui %.2f R$ de saldo",Bradesco.saldo);
+    System.out.print("Digite o valor a ser sacado: ");
+    String valorSaqueString = input.nextLine().replace(",",".");//caso o user digite uma virgula ao invés do ponto o programa faz a conversão
+    Double valorSaque = Double.parseDouble(valorSaqueString);
+    Double saldoTotal = Bradesco.saldo - valorSaque;
+
+    System.out.printf("Seu saldo após o saque: %.2f",saldoTotal);
+
+
+
+
+    }
+}
