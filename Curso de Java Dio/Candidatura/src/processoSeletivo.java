9c745516458e47e0afbb186bed902f6bc62c8c73
Elton <eltonpbelo01@gmail.com>
2023-10-06 08:06:15 -0300

    MUDANCA

--- Curso de Java Dio/Candidatura/src/processoSeletivo.java
+++ Curso de Java Dio/Candidatura/src/processoSeletivo.java
@@ -1,3 +1,22 @@
 public class processoSeletivo {
-    
+    public static void main(String[] args) {
+        
+        System.out.println("Candidatura");
+
+    }
+    static void analisarCandidato(Double salarioPretendido){
+
+        Double salarioBase = 2000.0;
+
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("OFERECER UMA CONTRAPROPOSTA");
+        }
+        
+        else{
+        System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS!");
+        }
+    }
 }
