9c7c6084cf37ccc3357b2f5cfc552750de646d24
Elton <eltonpbelo01@gmail.com>
2023-10-06 09:32:34 -0300

    new lines

--- Curso de Java Dio/Candidatura/src/processoSeletivo.java
+++ Curso de Java Dio/Candidatura/src/processoSeletivo.java
@@ -1,10 +1,34 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class processoSeletivo {
     public static void main(String[] args) {
         
     }
 
-    static void selecaoCandidatos(){}
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Messi","Neymar","Haaland","Ronaldo","Coutinho","Vini Jr","Lewadowski","Belligham","Rodrygo","Pedri"};
 
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        Double salarioBase = 2000.0;
+
+        while(candidatosSelecionados < 5){
+            String candidato = candidatos[candidatoAtual];
+            Double salarioPretendido = valorPretendido();
+            System.out.println("O candidato "+ candidato+" Solicitou esse salario "+salarioPretendido+"R$");
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidado "+candidato+" foi selecionado para vaga");
+                candidatosSelecionados++;
+
+            }
+            candidatoAtual++;
+        }
+
+       
+    }
+        static Double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800.0,2000.0);
+    }
 
     static void  analisarCandidato(Double salarioPretendido){ //função pra analisar candidato
 
