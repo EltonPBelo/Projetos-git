84fc96dcac07232b36e0b59b96b393a58dc3754e
Elton <eltonpbelo01@gmail.com>
2023-10-06 09:58:40 -0300

    aaaa

--- Curso de Java Dio/Candidatura/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Double a = 1000.00;
-        a.analisarCandidato;
-    }
-}
