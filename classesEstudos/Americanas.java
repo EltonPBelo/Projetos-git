630ee0ab7720d8bc66b8295a46d52df6fd6ae4d1
Elton <eltonpbelo01@gmail.com>
2023-10-13 11:14:47 -0300

    codigo full

--- classesEstudos/Americanas.java
+++ classesEstudos/Americanas.java
@@ -2,4 +2,8 @@ package classesEstudos;
 
 public class Americanas {
     
+    String eletronico;
+    Double preco;
+    Double desconto;
+
 }
--- classesEstudos/AmericanasTeste.java
+++ classesEstudos/AmericanasTeste.java
@@ -1,5 +1,67 @@
 package classesEstudos;
 
+import java.util.Scanner;
+
 public class AmericanasTeste {
-    
+    public static void main(String[] args) {
+        
+        Scanner coletarDados = new Scanner(System.in);
+
+        Americanas produtos = new Americanas();
+
+        produtos.eletronico = "Iphone 15";
+        produtos.preco = 8000.0;
+        produtos.desconto = 0.05;
+
+        Double produtoFinal = produtos.preco - (produtos.preco * produtos.desconto);
+
+        System.out.println(produtos.eletronico);
+        System.out.println(produtos.preco);
+        System.out.println(produtos.desconto);
+
+        
+        System.out.printf("O seu %s ficou por %.2f\n", produtos.eletronico, produtoFinal);
+
+        // vai armazenar sim || nao 
+        System.out.println("Deseja parcelar:  ");
+        String parcelamento = coletarDados.nextLine();
+
+
+        //vai armazenar a quantidade de percelamento parcelamento
+        System.out.println("Em quantas vezes deseja parcelar, parcelamos em até 12x com juros no parcelamento de 8x "); 
+        int respostaParcelamento = coletarDados.nextInt();
+
+        if (parcelamento.equalsIgnoreCase("sim") && respostaParcelamento >1 && respostaParcelamento < 12) {
+            Double cartao = produtoFinal/respostaParcelamento;
+            System.out.printf("O valor a ser paga do seu %s por mês por um total de %d meses é de %.2f",produtos.eletronico,respostaParcelamento, cartao);       
+        }
+
+        
+        else if(parcelamento.equalsIgnoreCase("nao") || parcelamento.equalsIgnoreCase("não")){
+            System.out.println("compra á vista efetuada com sucesso");
+        }
+
+        
+            
+            while (respostaParcelamento > 12 || respostaParcelamento < 1){
+                System.out.println("Você digitou um valor indisponivél de parcelamento tente de novo");
+                respostaParcelamento = coletarDados.nextInt();
+
+                if(respostaParcelamento > 0 || respostaParcelamento < 12){
+                Double cartao = produtoFinal/respostaParcelamento;
+                System.out.printf("O valor a ser paga do seu %s por mês por um total de %d meses é de %.2f",produtos.eletronico,respostaParcelamento, cartao);
+                }
+            }
+
+
+
+        coletarDados.close();
+    }
 }
+
+//if(respostaParcelamento > 12 || respostaParcelamento < 1){}
+//do {
+//respostaParcelamento = coletarDados.nextInt();
+//Double cartao = produtoFinal/respostaParcelamento;
+//System.out.printf("O valor a ser paga do seu %s por mês por um total de %d meses é de %.2f",produtos.eletronico,respostaParcelamento, cartao);
+//} while (respostaParcelamento > 12 || respostaParcelamento < 1);
