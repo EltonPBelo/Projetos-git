9cbac9099be8129110af4901b5cba0c59b43e31b
Elton <eltonpbelo01@gmail.com>
2023-11-02 21:35:39 -0300

    new updates

--- classesEstudos/Carro.java
+++ classesEstudos/Carro.java
@@ -6,17 +6,29 @@ Crie uma classe chamada Carro com atributos como modelo, marca, ano, cor etc.
 Implemente métodos para ligar o carro, desligar o carro, acelerar e frear. */
 
 public String Cor;
-private String nome:
+private String nomeVeiculo;
 private String AnoFabricacao;
 private Double Fipe;
-private Double Ipva;
 private boolean Motor;
 
+
+public void setNomeCarro(String nomeCarro){
+    this.nomeVeiculo = nomeCarro;
+}
+
+public String getNomeCarro(){
+    return this.nomeVeiculo;
+}
 
 public Carro(String cor){
     this.Motor = false;
     
 }
+
+public void setFipe(Double valFipe){
+    this.Fipe = valFipe;
+}
+
 public Double getFipe(){
     return this.Fipe;
 }
@@ -25,13 +37,23 @@ public void setAnoFabricacao(String anoCarro){
 this.AnoFabricacao = anoCarro;
 }
 public String getAnoFabricacao() {
-    System.out.println("O ano de fabricação do seu veiculo é "+this.AnoFabricacao);
+    //System.out.println("O ano de fabricação do seu veiculo é "+this.AnoFabricacao);
     return AnoFabricacao;
 }
 
-public void CalcularIpva(){
+public Double CalcularIpva(Double ipva){
 
-    
+    return this.Fipe*ipva;
+
+}
+
+public Double getIpva(){
+    return this.Fipe;
+}
+
+public void InformacaoCarro(){
+
+    System.out.println("O seu carro é um "+this.nomeVeiculo);
 
 }
 
--- classesEstudos/CarroTeste.java
+++ classesEstudos/CarroTeste.java
@@ -3,11 +3,22 @@ package classesEstudos;
 public class CarroTeste {
     public static void main(String[] args) {
         
-        Carro veiculo = new Carro("Azul",78000.0);
+        Carro v1 = new Carro("Preta");
 
-        veiculo.setAnoFabricacao("2016/2017");
-        veiculo.getAnoFabricacao();
-        veiculo.CalcularIpva();
+        v1.setNomeCarro("Corsa");
+        
+        //vai pegar o parametro do valor do meu carro
+        v1.setFipe(100000.0);
+
+        //vou coletar o dado coletado do setFipe e imprimir ele
+        v1.getFipe();
+        //System.out.println(v1.getFipe());
+        
+
+        //Vai imprimir o valor do meu ipva
+        System.out.println(v1.CalcularIpva(0.04)+"R$");
+        System.out.println(v1.getNomeCarro());
+
 
     }
 }
