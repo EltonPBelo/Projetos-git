ff73a0623b70ef9e3e9bd6bad074cea5e601d434
Elton <eltonpbelo01@gmail.com>
2023-11-08 08:14:24 -0300

    Comentei as funções

--- classesEstudos/Carro.java
+++ classesEstudos/Carro.java
@@ -13,60 +13,64 @@ private boolean Motor;
 private int velocimetro;
 private int velocidadeMaxima = 200;
 
-
-public void setNomeCarro(String nomeCarro){
-    this.nomeVeiculo = nomeCarro;
-}
-
-public String getNomeCarro(){
-    return this.nomeVeiculo;
-}
-
+//metodo construtor
 public Carro(String cor){
     this.Motor = false;
     
 }
+// metodo acessero set pra dizer o nome do carro
+public void setNomeCarro(String nomeCarro){
+    this.nomeVeiculo = nomeCarro;
+}
+//metodo pra puxar o nome do carro
+public String getNomeCarro(){
+    return this.nomeVeiculo;
+}
 
+//metodo acessor pra dizer o valor da fipe
 public void setFipe(Double valFipe){
     this.Fipe = valFipe;
 }
-
+//metodo acessor pra puxar o valor da fipe
 public Double getFipe(){
     return this.Fipe;
 }
-
+//metodo acessor pra dizer o ano de fabricação do veiculo
 public void setAnoFabricacao(String anoCarro){
 this.AnoFabricacao = anoCarro;
 }
+//metodo acessor pra puxar o ano defabricação do veiculo
 public String getAnoFabricacao() {
     //System.out.println("O ano de fabricação do seu veiculo é "+this.AnoFabricacao);
     return AnoFabricacao;
 }
-
+//função publica pra calcular o valor do ipva
 public Double CalcularIpva(Double ipva){
 
     return this.Fipe*ipva;
 
 }
-
+//metodo acessor pra puxar o ipva
 public Double getIpva(){
     return this.Fipe;
 }
-
+//função pra dar informação do carro
 public void InformacaoCarro(){
 
     System.out.println("O seu carro é um "+this.nomeVeiculo);
 
 }
-
+// função para ligar o carro
 public boolean LigarCarro(){
     return this.Motor = true;
 }
+
+//desliga carro
 public boolean DesligarCarro(){
     return this.Motor = false;
 
 }
-
+//função paraa acelerar o carro
 public void acelerarCarro (){
     if (Motor == false) {
         System.out.println("impossivel acelerar, o carro está desligado!");
